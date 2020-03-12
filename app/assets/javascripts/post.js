976675647940d3a614def226d5672820050a6bbe
Ovy95 <jackoverton21@hotmail.com>
2020-03-12 14:05:03 +0000

    Fixed loading j query error

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,33 +1,42 @@
-$(document).ready(function(){
+var callback = function(){
 
   $('form').submit(function(event){
 
     var newMessage = window.updateForm.getElementsByClassName("form-control") [0].value;
 
-    $.ajax({
-      type : 'POST',
-      url : 'post_update',
-      data : 'newMessage', 'postID'
-      dataType : 'json',
-      encode :true
-    });
+      $.ajax({
+        type : 'POST',
+        url : 'post_update',
+        data : 'newMessage',
+        dataType : 'json',
+        encode :true
+      });
 
-    .done(function(data){
-      console.log('hello');
-    });
+      done(function(data){
+        console.log('hello');
+      });
 
-    event.preventDefault();
+      event.preventDefault();
 
   });
 
-  function openForm(id) {
-    <!-- used global variable -->
-    postID = parseInt(id)
-    document.getElementById("updateForm").style.display = "block";
-  };
+    function openForm(id) {
+      <!-- used global variable -->
+      postID = parseInt(id)
+      document.getElementById("updateForm").style.display = "block";
+    };
 
-  function closeForm() {
-    document.getElementById("updateForm").style.display = "none";
-  };
+    function closeForm() {
+      document.getElementById("updateForm").style.display = "none";
+    };
 
-});
+};
+
+if (
+  document.readyState === "complete" ||
+  (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+callback();
+} else {
+document.addEventListener("DOMContentLoaded", callback);
+}
