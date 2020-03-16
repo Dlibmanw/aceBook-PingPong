1adc10b231ac458d620147ed9a6164b281d322f6
Dafna Libman <dafnalibman@gmail.com>
2020-03-16 15:01:19 +0000

    Add carrierwave and photos table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200312152429) do
+ActiveRecord::Schema.define(version: 20200316145044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
