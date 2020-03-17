d2c1b4a298d67f10959d884b0419db5ba6453afe
Josh Blumberg <jlblumberg@gmail.com>
2020-03-17 17:00:08 +0000

    generate user controller and comment out controller test

--- spec/features/user_can_view_photos_spec.rb
+++ spec/features/user_can_view_photos_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "View Photos", type: :feature do
     User.create(id: 1, name: 'Test Name', email: 'signin_test@example.com', password: '123456')
     Photo.create(image: 'image.png', user_id: 1)
     sign_up_with_name('test-user', 'test@example.com', '123456')
-    visit '/user/1'
-    expect(page).to have_content('test-user')
+    visit '/users/1'
+    expect(page).to have_content('Test Name')
   end
 end
