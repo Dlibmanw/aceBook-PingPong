412a3fd09f69c8f104767c7eefa6c0bd9e14178b
Josh Blumberg <jlblumberg@gmail.com>
2020-03-17 10:05:14 +0000

    Add comment for current test status

--- spec/features/user_can_upload_photos_spec.rb
+++ spec/features/user_can_upload_photos_spec.rb
@@ -8,12 +8,13 @@ RSpec.feature "Photo Album", type: :feature do
     expect(page).to have_link("New photo")
   end
 
-  # scenario "a user can view the new photo form" do
-  #   sign_up("josh@example.com", "1234567")
-  #   visit("/photos")
-  #   click_link("New photo")
-  #   expect(current_path).to eq("/photos/new")
-  #   expect(page).to have_selector(:link_or_button, 'Choose file')
-  #   # expect(page).to have_button("Create photo")
-  # end
+  scenario "a user can view the new photo form" do
+    sign_up("josh@example.com", "1234567")
+    visit("/photos")
+    click_link("New photo")
+    expect(current_path).to eq("/photos/new")
+    # the below line is failing
+    # expect(page).to have_button('photo_image')
+    # expect(page).to have_button("Create photo")
+  end
 end
