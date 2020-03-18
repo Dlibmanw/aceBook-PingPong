c0f4ed13c7bd14d9a9e1e7c61ff4153f5a236c12
Josh Blumberg <jlblumberg@gmail.com>
2020-03-18 14:12:37 +0000

    Updating user controller edit/update (WIP)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def edit 
-    @user = User.find(params[:id])
+  def edit
+    @user = User.find(current_user.id)
     @photo = Photo.find(params[:id])
   end 
 
   def update
     @user = User.update(photo_params)
-    redirect_to users_url
+    redirect_to user_url
   end
 
   private
 
   def photo_params
-    params.permit(:photo_id)
+    params.permit(:photo_id).merge(photo_id: params[:id])
   end
 
 end
