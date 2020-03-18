c2a0bd2ddac227a1785ea4383be67ab99bede0cc
Dafna Libman <dafnalibman@gmail.com>
2020-03-18 14:24:12 +0000

    Fix uploader error

--- spec/features/user_can_view_photos_spec.rb
+++ spec/features/user_can_view_photos_spec.rb
@@ -11,7 +11,7 @@ RSpec.feature "View Photos", type: :feature do
 
   scenario "View other users photos" do
     User.create(id: 1, name: 'Test Name', email: 'signin_test@example.com', password: '123456')
-    Photo.create(image: 'image.png', user_id: 1)
+    Photo.create(remote_image_url: 'image.png', user_id: 1)
     sign_up_with_name('test-user', 'test@example.com', '123456')
     visit '/users/1'
     expect(page).to have_content('Test Name')
