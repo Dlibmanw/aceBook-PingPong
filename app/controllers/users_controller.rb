fc953f2e6bfcde7a879c45ac93d4226b8eca85e8
Joe Ellingworth <joeellingworth@gmail.com>
2020-03-18 12:12:57 +0000

    Edits to photos controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit 
+    @photo = Photo.find(params[:id])
+  end 
+
+  def update
+    @user = User.update(photo_params)
+    redirect_to users_url
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:photo_id)
+  end
+
 end
