2f873b23b4e28fa3c040dfb43c4c220cd3c210eb
Joe Ellingworth <joeellingworth@gmail.com>
2020-03-19 11:03:38 +0000

    fixed profile photo selection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,27 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) 
-    @photo = Photo.find_by(user_id: current_user.id)
+    @profile_photo = Photo.find_by(id: @user.photo_id)
   end
 
   def edit
     @user = User.find(current_user.id)
-    @photo = Photo.find_by(user_id: current_user.id)
+    @photo = Photo.find(params[:format])
+    session[:photo_id_to_pass] = @photo.id
   end
 
   def update
     #"user"=>{"photo_id"=>""}
     #Need to update the photo ID coloum in the USERS table with the id of the phote they have selected
-    
-    @user = User.update(photo_params)
+    @user = User.find(current_user.id)
+    @user.update(photo_params)
     redirect_to user_url
   end
 
   private
 
   def photo_params
-    params.permit(:photo_id).merge(photo_id: Photo.find_by(user_id: current_user.id))
+    params.permit(:photo_id).merge(photo_id: session[:photo_id_to_pass])
   end
 
 end
