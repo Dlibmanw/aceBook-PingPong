a16cc5932b61b1a6b350e92b6c6ba3b2962df7fb
Joe Ellingworth <joeellingworth@gmail.com>
2020-03-19 11:12:31 +0000

    deleted bad test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    #"user"=>{"photo_id"=>""}
-    #Need to update the photo ID coloum in the USERS table with the id of the phote they have selected
     @user = User.find(current_user.id)
     @user.update(photo_params)
     redirect_to user_url
