e52f916003d6d5346a6f19ecee5cfd61aedccd62
Josh Blumberg <jlblumberg@gmail.com>
2020-03-22 18:48:17 +0000

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,6 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-    host:                  "s3-us-east-1.amazonaws.com",
-    region:                 "us-east-1"
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
