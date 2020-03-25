0f69979e8e5a1f2016408334a6c18c0935e258f8
Josh Blumberg <jlblumberg@gmail.com>
2020-03-25 11:45:25 +0000

    Add test v dev environment solutions to carrierwave.rb

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'pg'
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
 gem 'carrierwave', '~> 2.0'
-gem 'fog-aws'
+gem 'fog-aws', '3.6.2'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
@@ -52,6 +52,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
 gem 'devise'
 gem 'figaro'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,5 @@
 CarrierWave.configure do |config|
+
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
@@ -8,4 +9,15 @@ CarrierWave.configure do |config|
   }
   config.fog_directory = ENV["AWS_BUCKET"]              # required
   config.fog_public    = false
+  config.cache_dir = "#{Rails.root}/tmp/uploads"        # To let CarrierWave work on heroku
+
+  # store files locally in test environment
+  if Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+  end
+
 end
