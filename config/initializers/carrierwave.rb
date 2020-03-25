c96b3b529319798159711b04c6ce8ab7fb70a8e6
Josh Blumberg <jlblumberg@gmail.com>
2020-03-25 14:06:24 +0000

    Update rubocop yml to tolerate rails root binding

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,14 @@
 CarrierWave.configure do |config|
 
-  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    provider:              'AWS',                            # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],            # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],            # required
     region:                'us-east-1'
   }
-  config.fog_directory = ENV["AWS_BUCKET"]              # required
+  config.fog_directory = ENV["AWS_BUCKET"]                   # required
   config.fog_public    = false
-  config.cache_dir = "#{Rails.root}/tmp/uploads"        # To let CarrierWave work on heroku
+  config.cache_dir = "#{Rails.root}/tmp/uploads"             # To let CarrierWave work on heroku
 
   # store files locally in test environment
   if Rails.env.test?
